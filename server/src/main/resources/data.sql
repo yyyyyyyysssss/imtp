a3c2ea9a59cb92bf946962acb7ebb8f6bded652e
ys <Shuai.Yan@keyrus.com>
2024-04-29 18:09:04 +0800

    初始化表结构以及数据

--- server/src/main/resources/data.sql
+++ server/src/main/resources/data.sql
@@ -1,3 +1,3 @@
-insert into u_user(username,password,name) values('147',123456,'大鱼');
-insert into u_user(username,password,name) values('258',123456,'！');
-insert into u_user(username,password,name) values('369',123456,'。');
+INSERT INTO `im`.`im_user`(`id`, `username`, `password`, `nickname`, `gender`, `avatar`, `create_time`) VALUES (1, '147', '123456', '大鱼', '男', NULL, '2024-04-28 18:02:18');
+INSERT INTO `im`.`im_user`(`id`, `username`, `password`, `nickname`, `gender`, `avatar`, `create_time`) VALUES (2, '258', '123456', '小鱼', '女', NULL, '2024-04-28 18:02:44');
+INSERT INTO `im`.`im_user`(`id`, `username`, `password`, `nickname`, `gender`, `avatar`, `create_time`) VALUES (3, '369', '123456', '!', '男', NULL, '2024-04-28 18:03:04');
--- server/src/main/resources/schema.sql
+++ server/src/main/resources/schema.sql
@@ -1,31 +1,84 @@
-create table U_USER
-(
-    ID BIGINT auto_increment primary key,
-    USERNAME CHARACTER VARYING(48) not null unique,
-    PASSWORD CHARACTER VARYING(48),
-    NAME     CHARACTER VARYING(24)
-);
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
 
-create table G_CHAT
-(
-    ID   BIGINT auto_increment primary key,
-    NAME CHARACTER VARYING(48)
-);
+-- ----------------------------
+-- Table structure for im_group
+-- ----------------------------
+DROP TABLE IF EXISTS `im_group`;
+CREATE TABLE `im_group`  (
+                             `id` bigint(0) NOT NULL,
+                             `name` varchar(48) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '群名称',
+                             `create_user_id` bigint(0) NULL DEFAULT NULL COMMENT '创建人id',
+                             `create_time` datetime(0) NULL DEFAULT NULL COMMENT '创建时间',
+                             `state` tinyint(1) NULL DEFAULT NULL COMMENT '群组状态 0  已解散 1 正常',
+                             PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
-create table O_MSG
-(
-    ID        BIGINT auto_increment primary key,
-    SENDER    BIGINT  not null,
-    RECEIVER  BIGINT  not null,
-    TIMESTAMP BIGINT  not null,
-    TYPE      INTEGER not null,
-    STATUS    INTEGER not null,
-    MSG       CHARACTER VARYING(2048)
-);
+-- ----------------------------
+-- Table structure for im_group_user
+-- ----------------------------
+DROP TABLE IF EXISTS `im_group_user`;
+CREATE TABLE `im_group_user`  (
+                                  `id` bigint(0) NOT NULL,
+                                  `group_id` bigint(0) NULL DEFAULT NULL COMMENT '关联群组id',
+                                  `user_id` bigint(0) NULL DEFAULT NULL COMMENT '关联用户id',
+                                  `join_time` datetime(0) NULL DEFAULT NULL COMMENT '加入时间',
+                                  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
-create table U_GROUP
-(
-    ID       BIGINT auto_increment primary key,
-    USER_ID  BIGINT not null,
-    GROUP_ID BIGINT not null
-);
+-- ----------------------------
+-- Table structure for im_msg
+-- ----------------------------
+DROP TABLE IF EXISTS `im_msg`;
+CREATE TABLE `im_msg`  (
+                           `id` bigint(0) NOT NULL COMMENT '主键',
+                           `sender_user_id` bigint(0) NULL DEFAULT NULL COMMENT '关联发送人用户id',
+                           `receiver_user_id` bigint(0) NULL DEFAULT NULL COMMENT '关联接收人用户id',
+                           `type` int(0) NULL DEFAULT NULL COMMENT '消息类型',
+                           `content` varchar(2048) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '消息内容',
+                           `send_time` datetime(0) NULL DEFAULT NULL COMMENT '发送时间',
+                           PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for im_offline_msg
+-- ----------------------------
+DROP TABLE IF EXISTS `im_offline_msg`;
+CREATE TABLE `im_offline_msg`  (
+                                   `id` bigint(0) NOT NULL,
+                                   `msg_id` bigint(0) NULL DEFAULT NULL COMMENT '关联消息id',
+                                   `user_id` bigint(0) NULL DEFAULT NULL COMMENT '关联用户id',
+                                   `state` tinyint(1) NULL DEFAULT NULL COMMENT '消息状态 0 待推送 1 已推送',
+                                   PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for im_user
+-- ----------------------------
+DROP TABLE IF EXISTS `im_user`;
+CREATE TABLE `im_user`  (
+                            `id` bigint(0) NOT NULL,
+                            `username` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '登录账号',
+                            `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '密码',
+                            `nickname` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '昵称',
+                            `gender` varchar(12) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '性别',
+                            `avatar` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '头像',
+                            `create_time` datetime(0) NULL DEFAULT NULL COMMENT '创建时间',
+                            PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for im_user_friend
+-- ----------------------------
+DROP TABLE IF EXISTS `im_user_friend`;
+CREATE TABLE `im_user_friend`  (
+                                   `id` bigint(0) NOT NULL,
+                                   `user_id` bigint(0) NULL DEFAULT NULL COMMENT '关联用户id',
+                                   `friend_id` bigint(0) NULL DEFAULT NULL COMMENT '关联好友id',
+                                   `state` tinyint(1) NULL DEFAULT NULL COMMENT '好友状态 0 删除 1正常',
+                                   `create_time` datetime(0) NULL DEFAULT NULL COMMENT '创建时间',
+                                   PRIMARY KEY (`id`) USING BTREE,
+                                   INDEX `user_id_idx`(`user_id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
