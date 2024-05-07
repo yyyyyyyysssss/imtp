07044c20f5e0421662944f3c526cef2aac32d9d5
ys <Shuai.Yan@keyrus.com>
2024-05-07 18:15:16 +0800

    客户端优化

--- client/src/main/java/org/imtp/client/model/LoginModel.java
+++ client/src/main/java/org/imtp/client/model/LoginModel.java
@@ -2,7 +2,7 @@ package org.imtp.client.model;
 
 import io.netty.channel.ChannelHandler;
 
-public interface LoginModel extends Model<Void>{
+public interface LoginModel extends Model{
 
     void ready(ChannelHandler channelHandler);
 
