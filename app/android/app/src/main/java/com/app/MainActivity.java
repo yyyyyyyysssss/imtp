c5ff557a1828e9a5c27bf12e878b1abd61e4f00b
ys <Shuai.Yan@keyrus.com>
2024-11-24 13:20:12 +0800

    app模块添加

---
+++ app/android/app/src/main/java/com/app/MainActivity.java
@@ -0,0 +1,19 @@
+package com.app;
+
+import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
+import com.facebook.react.defaults.DefaultReactActivityDelegate;
+
+public class MainActivity extends ReactActivity {
+
+    @Override
+    protected String getMainComponentName() {
+        return "app";
+    }
+
+    @Override
+    protected ReactActivityDelegate createReactActivityDelegate() {
+        return new DefaultReactActivityDelegate(this,"app", DefaultNewArchitectureEntryPoint.getFabricEnabled());
+    }
+}
---
+++ app/android/app/src/main/java/com/app/MainApplication.java
@@ -0,0 +1,79 @@
+package com.app;
+
+import static com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.facebook.react.PackageList;
+import com.facebook.react.ReactApplication;
+import com.facebook.react.ReactHost;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
+import com.facebook.react.defaults.DefaultReactNativeHost;
+import com.facebook.react.soloader.OpenSourceMergedSoMapping;
+import com.facebook.soloader.SoLoader;
+
+import java.io.IOException;
+import java.util.List;
+
+public class MainApplication extends Application implements ReactApplication {
+
+
+    private final ReactNativeHost reactNativeHost = new DefaultReactNativeHost(this) {
+
+        @Override
+        protected List<ReactPackage> getPackages() {
+            return new PackageList(this).getPackages();
+        }
+
+        @Override
+        public boolean getUseDeveloperSupport() {
+            return BuildConfig.DEBUG;
+        }
+
+        @Override
+        protected String getJSMainModuleName() {
+            return "index";
+        }
+
+        @Override
+        protected Boolean isHermesEnabled() {
+            return BuildConfig.IS_HERMES_ENABLED;
+        }
+
+        @Override
+        protected boolean isNewArchEnabled() {
+            return BuildConfig.IS_NEW_ARCHITECTURE_ENABLED;
+        }
+    };
+
+    @Nullable
+    @Override
+    public ReactHost getReactHost() {
+        return getDefaultReactHost(getApplicationContext(), reactNativeHost);
+    }
+
+    @NonNull
+    @Override
+    public ReactNativeHost getReactNativeHost() {
+        return reactNativeHost;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        try {
+            SoLoader.init(this, OpenSourceMergedSoMapping.INSTANCE);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
+            // If you opted-in for the New Architecture, we load the native entry point for this app.
+            DefaultNewArchitectureEntryPoint.load();
+        }
+    }
+}
