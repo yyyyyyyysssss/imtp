98f349e17166accbca98600703380a7d97170b33
yanshuai <yan.shuai.ext@jx.t3go.cn>
2025-03-27 17:24:59 +0800

    配置还原

--- web-ui/src/env.js
+++ web-ui/src/env.js
@@ -1,7 +1,7 @@
 // 全局环境变量
 const env = {
-    apiUrl: 'http://192.168.31.41:9090',
-    wobsocketUrl: 'ws://192.168.31.41:8080/im'
+    apiUrl: 'http://127.0.0.1:9090',
+    wobsocketUrl: 'ws://127.0.0.1:8080/im'
 }
 
 export default env;
