eac44cd306c43d5dbdc661ea9e07070875ab1bb4
yanshuai <yan.shuai.ext@jx.t3go.cn>
2025-05-15 15:07:03 +0800

    api graalvm config

--- api/src/main/java/org/imtp/api/config/ApiRuntimeHintsConfig.java
+++ api/src/main/java/org/imtp/api/config/ApiRuntimeHintsConfig.java
@@ -1,5 +1,10 @@
 package org.imtp.api.config;
 
+import org.springframework.aot.hint.RuntimeHints;
+import org.springframework.aot.hint.RuntimeHintsRegistrar;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportRuntimeHints;
+
 /**
  * @Description
  * @Author ys
