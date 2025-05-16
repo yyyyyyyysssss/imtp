9c662732378b5a6be02678e9a7a7876b3ad4aa1f
yanshuai <yan.shuai.ext@jx.t3go.cn>
2025-05-16 11:08:11 +0800

    graalvm config

--- api/src/main/java/org/imtp/api/config/ApiRuntimeHintsConfig.java
+++ api/src/main/java/org/imtp/api/config/ApiRuntimeHintsConfig.java
@@ -1,9 +1,11 @@
 package org.imtp.api.config;
 
+import org.springframework.aot.hint.MemberCategory;
 import org.springframework.aot.hint.RuntimeHints;
 import org.springframework.aot.hint.RuntimeHintsRegistrar;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportRuntimeHints;
+import org.springframework.security.authentication.ProviderManager;
 
 /**
  * @Description
@@ -16,7 +18,7 @@ public class ApiRuntimeHintsConfig implements RuntimeHintsRegistrar {
     @Override
     public void registerHints(RuntimeHints hints, ClassLoader classLoader) {
         //反射
-        
+        hints.reflection().registerType(ProviderManager.class, MemberCategory.INVOKE_DECLARED_CONSTRUCTORS);
         //资源
         hints.resources().registerPattern("config/application.yaml");
     }
