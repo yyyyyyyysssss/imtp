b6e45411b87dc6ad4216f2a9edee15ceafd7c4ff
ys <ysyanshuai2@gmail.com>
2025-08-02 14:08:12 +0800

    文件访问接口

---
+++ api/src/main/java/org/imtp/api/controller/FileAccessController.java
@@ -0,0 +1,66 @@
+package org.imtp.api.controller;
+
+
+import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
+import org.imtp.api.config.exception.BusinessException;
+import org.imtp.api.domain.dto.FileRangeDTO;
+import org.imtp.api.domain.vo.FileStreamVO;
+import org.imtp.api.service.FileService;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
+
+import java.util.Map;
+import java.util.Objects;
+
+@RequestMapping("/file")
+@RestController
+@Slf4j
+public class FileAccessController {
+
+    @Resource(name = "minioFileService") // 使用本地文件服务
+    private FileService fileService;
+
+    //获取文件
+    @GetMapping("/{bucketName}/{objectName}")
+    public ResponseEntity<StreamingResponseBody> getFile(@PathVariable("bucketName") String bucketName,
+                                                         @PathVariable("objectName") String objectName,
+                                                         @RequestParam(required = false,value = "type") String type,
+                                                         @RequestHeader(value = HttpHeaders.RANGE, required = false) String range) {
+        HttpHeaders httpHeaders = new HttpHeaders();
+        FileRangeDTO fileRangeDTO = parseRange(range);
+        FileStreamVO fileStream = fileService.getFileStream(bucketName, objectName, fileRangeDTO);
+        StreamingResponseBody streamingResponseBody = fileStream.getStreamingResponseBody();
+        Map<String, String> headerMap = fileStream.getHeaders();
+        if(headerMap != null && !headerMap.isEmpty()) {
+            // 将文件头信息添加到响应头中
+            headerMap.forEach(httpHeaders::add);
+        }
+        if(type != null && (type.equalsIgnoreCase("download") || type.equalsIgnoreCase("d"))) {
+            // 设置响应头以指示下载
+            httpHeaders.setContentDispositionFormData("attachment", objectName);
+            httpHeaders.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+        }
+        return ResponseEntity.ok()
+                .headers(httpHeaders)
+                .body(streamingResponseBody);
+    }
+
+    private FileRangeDTO parseRange(String range) {
+        if(range != null && !range.isEmpty()) {
+            String[] ranges = range.replace("bytes=", "").split(",");
+            if(ranges.length > 1){
+                throw new BusinessException("暂不支持多范围请求");
+            }
+            String[] limits = ranges[0].split("-");
+            long start = Objects.equals(limits[0], "") ? 0 : Long.parseLong(limits[0]);
+            long end = limits.length > 1 ? Long.parseLong(limits[1]) : -1;
+            return new FileRangeDTO(start, end);
+        }
+        return null;
+    }
+
+}
--- api/src/main/java/org/imtp/api/controller/FileUploadController.java
+++ api/src/main/java/org/imtp/api/controller/FileUploadController.java
@@ -2,27 +2,19 @@ package org.imtp.api.controller;
 
 import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
-import org.imtp.api.config.exception.BusinessException;
 import org.imtp.api.domain.dto.FileChunkDTO;
 import org.imtp.api.domain.dto.FileInfoDTO;
-import org.imtp.api.domain.dto.FileRangeDTO;
 import org.imtp.api.domain.vo.FileInfoVO;
-import org.imtp.api.domain.vo.FileStreamVO;
 import org.imtp.api.domain.vo.FileUploadChunkVO;
 import org.imtp.api.domain.vo.FileUploadProgressVO;
 import org.imtp.api.service.FileService;
 import org.imtp.common.response.Result;
 import org.imtp.common.response.ResultGenerator;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
 
 import java.time.Duration;
-import java.util.Map;
-import java.util.Objects;
 
 /**
  * @Description
@@ -32,7 +24,7 @@ import java.util.Objects;
 @RequestMapping("/api/file")
 @RestController
 @Slf4j
-public class FileController {
+public class FileUploadController {
 
     @Resource(name = "minioFileService") // 使用本地文件服务
     private FileService fileService;
@@ -81,46 +73,6 @@ public class FileController {
     public Result<FileInfoVO> fileInfo(@PathVariable("bucketName") String bucketName, @PathVariable("objectName") String objectName) {
         FileInfoVO fileInfo = fileService.getFileInfo(bucketName, objectName);
         return ResultGenerator.ok(fileInfo);
-    }
-
-    //获取文件
-    @GetMapping("/{bucketName}/{objectName}")
-    public ResponseEntity<StreamingResponseBody> getFile(@PathVariable("bucketName") String bucketName,
-                                                         @PathVariable("objectName") String objectName,
-                                                         @RequestParam(required = false,value = "type") String type,
-                                                         @RequestHeader(value = HttpHeaders.RANGE, required = false) String range) {
-        HttpHeaders httpHeaders = new HttpHeaders();
-        FileRangeDTO fileRangeDTO = parseRange(range);
-        FileStreamVO fileStream = fileService.getFileStream(bucketName, objectName, fileRangeDTO);
-        StreamingResponseBody streamingResponseBody = fileStream.getStreamingResponseBody();
-        Map<String, String> headerMap = fileStream.getHeaders();
-        if(headerMap != null && !headerMap.isEmpty()) {
-            // 将文件头信息添加到响应头中
-            headerMap.forEach(httpHeaders::add);
-        }
-        if(type != null && (type.equalsIgnoreCase("download") || type.equalsIgnoreCase("d"))) {
-            // 设置响应头以指示下载
-            httpHeaders.setContentDispositionFormData("attachment", objectName);
-            httpHeaders.setContentType(MediaType.APPLICATION_OCTET_STREAM);
-        }
-        return ResponseEntity.ok()
-                .headers(httpHeaders)
-                .body(streamingResponseBody);
-    }
-
-
-    private FileRangeDTO parseRange(String range) {
-        if(range != null && !range.isEmpty()) {
-            String[] ranges = range.replace("bytes=", "").split(",");
-            if(ranges.length > 1){
-                throw new BusinessException("暂不支持多范围请求");
-            }
-            String[] limits = ranges[0].split("-");
-            long start = Objects.equals(limits[0], "") ? 0 : Long.parseLong(limits[0]);
-            long end = limits.length > 1 ? Long.parseLong(limits[1]) : -1;
-            return new FileRangeDTO(start, end);
-        }
-        return null;
     }
 
 }
